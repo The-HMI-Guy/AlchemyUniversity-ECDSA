962b72197ffa716d0b0f7ae5f1d58806387eaa34
Ray Ortiz <oray@bgsu.edu>
2022-12-05 07:54:44 -0500

    todo section added

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  //TODO: get a signature from the client-side app
+  //      recover the public address from the signature
+  //      This would be the sender.
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
